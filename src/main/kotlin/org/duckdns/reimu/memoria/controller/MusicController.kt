ec46a0204a5efe26146b7481287cf10bb1dec15e
lkz9375 <extra683@naver.com>
2022-04-24 17:36:48 +0900

    컨트롤러 작성

--- src/main/kotlin/org/duckdns/reimu/memoria/controller/MusicController.kt
+++ src/main/kotlin/org/duckdns/reimu/memoria/controller/MusicController.kt
@@ -1,4 +1,23 @@
 package org.duckdns.reimu.memoria.controller
 
-class MusicController {
+import org.duckdns.reimu.memoria.service.MusicService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/music")
+class MusicController(
+    private val musicService: MusicService
+) {
+    @GetMapping("")
+    fun getMusicList(
+        model: Model
+    ): String {
+        val musicList = musicService.getList()
+        model.addAttribute("musicList", musicList)
+
+        return "music/index"
+    }
 }
--- src/main/kotlin/org/duckdns/reimu/memoria/service/MusicService.kt
+++ src/main/kotlin/org/duckdns/reimu/memoria/service/MusicService.kt
@@ -5,11 +5,17 @@ import org.duckdns.reimu.memoria.model.param.MusicParam
 import org.duckdns.reimu.memoria.repository.MusicRepository
 import org.duckdns.reimu.memoria.util.RequestUtils
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class MusicService(
     private val musicRepository: MusicRepository,
 ) {
+    fun getList(): List<Music> {
+        return musicRepository.findAll()
+    }
+
+    @Transactional
     fun add(musicParam: MusicParam): Music {
         val musicDto = RequestUtils.makeMusicFrom(musicParam.url)
         return musicRepository.save(musicDto.toEntity())
