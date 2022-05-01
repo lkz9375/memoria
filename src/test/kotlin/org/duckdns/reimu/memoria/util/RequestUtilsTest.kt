c15b89ee448d28fc53ccc1b6112a71ef70f6745d
lkz9375 <extra683@naver.com>
2022-05-01 19:13:04 +0900

    태스트 코드 리팩토링

--- src/test/kotlin/org/duckdns/reimu/memoria/util/RequestUtilsTest.kt
+++ src/test/kotlin/org/duckdns/reimu/memoria/util/RequestUtilsTest.kt
@@ -1,32 +1,22 @@
 package org.duckdns.reimu.memoria.util
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class RequestUtilsTest {
-    @Test
-    fun jsoupTest1() {
-        val url = "https://www.youtube.com/watch?v=sFoWYa6QNKk&list=LL&index=79"
+    @ParameterizedTest
+    @ValueSource(strings = [
+        "https://www.youtube.com/watch?v=sFoWYa6QNKk&list=LL&index=79",
+        "https://www.youtube.com/watch?v=sFoWYa6QNKk",
+        "https://youtu.be/sFoWYa6QNKk"
+    ])
+    fun jsoupTest(url: String) {
         val musicDto = RequestUtils.makeMusicFrom(url)
 
         assertEquals(123, musicDto.length)
-        assertEquals("https://www.youtube.com/watch?v=sFoWYa6QNKk", musicDto.url)
+        assertEquals("sFoWYa6QNKk", musicDto.urlId)
         assertEquals("『ぎゅって』feat. 初音ミク", musicDto.title)
-        assertEquals("MIMI", musicDto.artist)
         assertEquals("2022-04-23", musicDto.uploaded.toString())
-        assertEquals("https://i.ytimg.com/vi/sFoWYa6QNKk/hqdefault.jpg", musicDto.thumbnailUrl)
-    }
-
-    @Test
-    fun jsoupTest2() {
-        val url = "https://www.youtube.com/watch?v=sFoWYa6QNKk"
-        val musicDto = RequestUtils.makeMusicFrom(url)
-
-        assertEquals(123, musicDto.length)
-        assertEquals("https://www.youtube.com/watch?v=sFoWYa6QNKk", musicDto.url)
-        assertEquals("『ぎゅって』feat. 初音ミク", musicDto.title)
-        assertEquals("MIMI", musicDto.artist)
-        assertEquals("2022-04-23", musicDto.uploaded.toString())
-        assertEquals("https://i.ytimg.com/vi/sFoWYa6QNKk/hqdefault.jpg", musicDto.thumbnailUrl)
     }
 }
